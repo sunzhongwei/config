36575c6bb3c494475e846003fdfe531795f98e9c
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2012-06-29 16:14:15 +0800

    deploy local

---
+++ deploy_local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ---------------------------------------- 
+# 功能介绍
+# 将代码库中的配置文件复制到指定位置
+# ---------------------------------------- 
+cp vimrc ~/.vimrc
+cp bash_profile ~/.bash_profile
+cp tmux.conf ~/.tmux.conf
+cp gitconfig ~/.gitconfig
+
