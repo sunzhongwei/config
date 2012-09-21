40b23a170ae040ffdad1e7d9ead1ebc82cf997e9
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2012-09-21 16:16:00 +0800

    vimwiki

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# ---------------------------------------- 
+# ----------------------------------------
 # 功能介绍
 # 将代码库中的配置文件复制到指定位置
-# ---------------------------------------- 
+# ----------------------------------------
 cp vimrc ~/.vimrc
 cp bash_profile ~/.bash_profile
 cp tmux.conf ~/.tmux.conf
 cp gitconfig ~/.gitconfig
+cp -Rf vim/* ~/.vim/
+
+echo "if you are using Ubuntu, romove ~/.bash_profile"
 
