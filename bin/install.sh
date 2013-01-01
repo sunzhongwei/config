49e66945a2d4d69db4afd2e003a97fe3ebe9958b
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-01-01 16:05:17 +0800

    install

---
+++ bin/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ----------------------------------------
+# DESCRIPTION
+# sudo ./install.sh
+# ----------------------------------------
+set -e	# or use "set -o errexit" to quit on error.
+set -x  # or use "set -o xtrace" to print the statement before you execute it.
+
+
+# make sure use sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: Must be run with root privileges."
+    exit 1
+fi
+
+
+APTITUDE_OPTIONS="-y"
+apt-get update
+cat <<PACKAGES | xargs apt-get install $APTITUDE_OPTIONS
+gcc
+make
+libevent-dev
+libncurses5-dev
+tmux
+nginx
+spawn-fcgi
+python-flup
+python-pip
+PACKAGES
+
+
