9adbd3585b7f79713e1ef930280c0db8ef7587c6
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-01-01 22:35:08 +0800

    update

--- deploy_local.sh
+++ deploy_local.sh
@@ -9,8 +9,12 @@ cp bash_profile ~/.bash_profile
 cp tmux.conf ~/.tmux.conf
 cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
+
+mkdir -p ~/.vim
 cp -Rf vim/* ~/.vim/
+mkdir -p ~/Templates
 cp -f Templates/* ~/Templates/
+mkdir -p ~/bin
 cp -f bin/* ~/bin/
 
 echo "if you are using Ubuntu, romove ~/.bash_profile"
