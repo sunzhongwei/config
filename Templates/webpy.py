b15eefe8e7f5565a2b6e0b5bf943c30dee4e1789
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-01-20 14:59:31 +0800

    update

---
+++ Templates/webpy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# build-in, 3rd party and my modules
+import web
+
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        return 'Hello, ' + name + '!'
+
+if __name__ == "__main__":
+    app.run()
+
+
--- vimrc
+++ vimrc
@@ -353,3 +353,9 @@ let g:vimwiki_list = [{'path': '~/vimwiki/',
   \ 'template_default': 'default_template',
   \ 'template_ext': '.html'}]
 
+let g:vimwiki_diary_months = {
+      \ 1: '一月', 2: '二月', 3: '三月', 
+      \ 4: '四月', 5: '五月', 6: '六月',
+      \ 7: '七月', 8: '八月', 9: '九月',
+      \ 10: '十月', 11: '十一月', 12: '十二月'
+      \ }
