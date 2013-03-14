4deec8488dbc05370f91a7b7734e5b7056cf491f
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-03-14 14:26:45 +0800

    1. fix sqlalchemy usage bug; 2. add error code

--- Templates/webpy/webpy.py
+++ Templates/webpy/webpy.py
@@ -21,10 +21,11 @@
 # build-in, 3rd party and my modules
 import os
 import web
+import json
 import os.path
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 from wsgilog import WsgiLog
@@ -33,19 +34,13 @@ from wsgilog import WsgiLog
 # ----------------------------------------
 # settings
 # ----------------------------------------
-LOG_FILE = "/tmp/webpy.log"
+LOG_FILE = "/tmp/webpy.log"     # use full path
 LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
 LOG_BACKUPS = 10
 
 sqlite_db_path = "/tmp/webpy.sqlite"
 engine = create_engine("sqlite:///%s" % sqlite_db_path, echo=True)
 Model = declarative_base()
-
-# create a configured "Session" class
-Session = sessionmaker(bind=engine)
-
-# create a Session
-session = Session()
 
 
 # ----------------------------------------
@@ -69,11 +64,31 @@ class Log(WsgiLog):
             datefmt = "%Y-%m-%d %H:%M:%S",
             )
 
+def load_sqla(handler):
+    '''http://webpy.org/cookbook/sqlalchemy
+    '''
+    web.ctx.orm = scoped_session(sessionmaker(bind=engine))
+    try:
+        return handler()
+    except web.HTTPError:
+       web.ctx.orm.commit()
+       raise
+    except:
+        web.ctx.orm.rollback()
+        raise
+    finally:
+        web.ctx.orm.commit()
+        # If the above alone doesn't work, uncomment
+        # the following line:
+        #web.ctx.orm.expunge_all()
 
 urls = (
-    '/(.*)', 'hello',
+    "/todos", "TodosHandler",
+    "/todos/(\d+)", "TodoHandler",
+    '/(.*)', 'HelloHandler',
 )
 app = web.application(urls, globals())
+app.add_processor(load_sqla)
 
 
 # ----------------------------------------
@@ -94,11 +109,74 @@ def create_tables():
 # ----------------------------------------
 # controller
 # ----------------------------------------
-class hello:
+class HelloHandler:
     def GET(self, name):
         if not name:
             name = 'World'
         return 'Hello, ' + name + '!'
+
+class TodosHandler:
+    def GET(self):
+        todos = web.ctx.orm.query(Todo).all()
+        result = {"data": []}
+        for todo in todos:
+            result["data"].append({"id": todo.id, "content": todo.content})
+        return json.dumps(result)
+
+    def POST(self):
+        data = web.input()
+        todo = Todo(content=data.content)
+        web.ctx.orm.add(todo)
+        return json.dumps({})
+
+class TodoHandler:
+    def GET(self, todo_id):
+        todo = web.ctx.orm.query(Todo).filter_by(id=todo_id).one()
+        return json.dumps({"data": {"id": todo.id, "content": todo.content}})
+
+    def PUT(self, todo_id):
+        data = web.input()
+        todo = web.ctx.orm.query(Todo).filter_by(id=todo_id).one()
+        todo.content = data.content
+        return json.dumps({})
+
+    def DELETE(self, todo_id):
+        web.ctx.orm.query(Todo).filter_by(id=todo_id).delete()
+        return json.dumps({})
+
+
+# ----------------------------------------
+# errors
+# ----------------------------------------
+class BaseError(Exception):
+    ret = 0
+    errcode = 0
+    msg = "ok"
+
+    @property
+    def json_response(self):
+        response = {"ret": self.ret, "errcode": self.errcode,
+                "msg": self.msg}
+        return json.dumps(response)
+
+class Success(BaseError):
+    pass
+
+class BaseParamError(BaseError):
+    ret = 1
+    msg = "invalid parameters"
+
+class BaseAuthError(BaseError):
+    ret = 3
+    msg = "auth error"
+
+class BaseServerError(BaseError):
+    ret = 4
+    msg = "server error"
+
+class NotExistError(BaseParamError):
+    errcode = 1
+    msg = "not exist"
 
 
 if __name__ == "__main__":
