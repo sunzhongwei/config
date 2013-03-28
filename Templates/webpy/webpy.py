26108f961f9013b12c88765e39a7a65e76732178
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-03-28 17:39:10 +0800

    record post data in log file

--- Templates/webpy/webpy.py
+++ Templates/webpy/webpy.py
@@ -93,7 +93,7 @@ def loadhook(handler):
         # If the above alone doesn't work, uncomment
         # the following line:
         #web.ctx.orm.expunge_all()
-    print "url: %s; method: %s, data: %s, cost_time: %0.3fs; response: %s" % (
+    print "url: %s; method: %s; data: %s; cost_time: %0.3fs; response: %s" % (
             web.ctx.path, web.ctx.method, web.input(),
             end_time-start_time, ret)
     return ret
