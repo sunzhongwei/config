66d3b37b88dae149114b1412677828a384bf5903
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-04-24 18:48:37 +0800

    not copy bash_profile, if system is Ubuntu

--- deploy_local.sh
+++ deploy_local.sh
@@ -4,8 +4,14 @@
 # 功能介绍
 # 将代码库中的配置文件复制到指定位置
 # ----------------------------------------
+OS=`uname`
+
 cp vimrc ~/.vimrc
-cp bash_profile ~/.bash_profile
+if [ "$OS" = "Linux" ]; then
+    echo "Not copy bash_profile, because os is" $OS
+else
+    cp bash_profile ~/.bash_profile
+fi;
 cp tmux.conf ~/.tmux.conf
 cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
@@ -16,6 +22,4 @@ mkdir -p ~/Templates
 cp -Rf Templates/* ~/Templates/
 mkdir -p ~/bin
 cp -f bin/* ~/bin/
-
-echo "if you are using Ubuntu, romove ~/.bash_profile"
 
