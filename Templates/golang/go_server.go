de6c345a8e8ceba45a51c29130065e6d014827ad
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-10-10 13:26:25 +0800

    go server template

---
+++ Templates/golang/go_server.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2013 Zhongwei Inc.
+
+Author: Zhongwei Sun
+EMail:  zhongwei.sun2008@gmail.com
+
+How to test:
+$ for i in {1..200}; do nohup curl localhost:8888 >/dev/null 2>&1 & done
+*/
+
+package main 
+
+import (
+	"fmt"
+	"time"
+	"net/http"
+)
+
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Start at:", time.Now())
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Println("End at:", time.Now())
+}
+
+
+func main() {
+	port := 8888
+	fmt.Println("Start server on port:", port)
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+}
+
