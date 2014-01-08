597541ba2438d435255e0583ea60f18021d6f504
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2014-01-08 11:00:50 +0800

    url encode

--- Templates/python/hello_url.py
+++ Templates/python/hello_url.py
@@ -10,6 +10,7 @@ __author__ = "Zhongwei Sun (zhongwei.sun2008@gmail.com)"
 
 # build-in, 3rd party and my modules
 from urlparse import urlparse, parse_qs
+from urllib import quote_plus, quote
 
 
 def test():
@@ -23,6 +24,18 @@ def test():
     print parse_qs(query)
 
 
+def test_url_encode():
+    '''http://www.w3schools.com/tags/ref_urlencode.asp
+    由于 url 只支持 ascii 编码，所以需要转换。
+    '''
+    url = "next=/#/details/www.test.com"
+    print "origin url is: %s" % url
+
+    print "urllib.quote result:\n%s" % quote(url)
+    print "urllib.quote_plus (which will quote slash) result:\n%s" % quote_plus(url)
+
+
 if '__main__' == __name__:
     test()
+    test_url_encode()
 
