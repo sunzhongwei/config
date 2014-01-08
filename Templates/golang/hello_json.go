37a1399cdf674dfc67932dbb2ee333446694fa65
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2014-01-08 18:43:36 +0800

    update

---
+++ Templates/golang/hello_json.go
@@ -0,0 +1,16 @@
+/*
+Copyright 2013 Zhongwei Inc.
+
+Author: Zhongwei Sun
+EMail:  zhongwei.sun2008@gmail.com
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
